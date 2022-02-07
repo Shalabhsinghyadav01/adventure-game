cb8f65840a74743cc64596d21367440163b87d84
Shalabhsinghyadav01 <shalabhsinghyadav@gmail.com>
2022-02-07 20:55:22 +0530

    changes

--- adventure_main.py
+++ adventure_main.py
@@ -26,26 +26,43 @@ print("Your mission is to find the treasure.")
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 #Write your code below this line 👇
-ques_first = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right"')
+ques_first = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right"').lower()
 # print(ques_first)
-if ques_first == "right":
-  print("You fell into a hole. Game Over.")
-elif ques_first == "left":
-  ques_first = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
-  # print(ques_first)
-  if ques_first == "swim":
-    print("You get attacked by an angry trout. Game Over.")
-  elif ques_first == "wait":
-     ques_first = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
-    #  print(ques_first)
-     if ques_first == "red":
+if ques_first == "left":
+  ques_secound = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.').lower()
+  if ques_secound == "wait":
+    ques_third = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?").loweer()
+    if ques_third == "red":
       print("It's a room full of fire. Game Over.")
-     elif ques_first == "blue":
+    elif ques_third == "blue":
       print("You enter a room of beasts. Game Over.")
-     elif ques_first == "yellow":
+    elif ques_third == "yellow":
       print("You found the treasure! You Win!")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+
+  else:
+    print("You get attacked by an angry trout. Game Over.")
 else:
-  print("You chose a door that doesn't exist. Game Over.")
+  print("You fell into a hole. Game Over.")
+#   # print(ques_first)
+#   print("You fell into a hole. Game Over.")
+# elif ques_first == "left":
+#   ques_first = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
+#   # print(ques_first)
+#   if ques_first == "swim":
+#     print("You get attacked by an angry trout. Game Over.")
+#   elif ques_first == "wait":
+#      ques_first = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
+#     #  print(ques_first)
+#      if ques_first == "red":
+#       print("It's a room full of fire. Game Over.")
+#      elif ques_first == "blue":
+#       print("You enter a room of beasts. Game Over.")
+#      elif ques_first == "yellow":
+#       print("You found the treasure! You Win!")
+# else:
+#   print("You chose a door that doesn't exist. Game Over.")
 
 
 
